bbf1e086a4995a99dabee114b5653d16f3534157
Dylan Jaoui <dylan.jaoui@ensta-paris.fr>
2020-06-09 11:19:55 +0200

    Ajout d'une deuxième particule dans la méthode intégrale

--- separation.py
+++ separation.py
@@ -9,8 +9,8 @@ from scipy.integrate import odeint
 
 H=50e-6                                          #Hauteur du tube
 mu = 0.001 #
-r=20e-6                                           #Taille particule 1
-r2=15e-6                                          #Taille particule 2
+r=15e-6                                           #Taille particule 1
+r2=10e-6                                          #Taille particule 2
 a=250e-6/2/np.pi                                  #Espace entre chaque tour de la spirale
 m=1050*4/3*np.pi*(r/2)**3                         #Masse particule
 rho=1000
@@ -24,7 +24,7 @@ vphi=0.27                                         #Vitesse constante longitudina
 dt=0.00001                                        #Pas de temps
 vr=0                                              #Vitesse latérale initiale de la particule 1
 vr2=0                                             #Vitesse latérale initiale de la particule 2
-pr0=2*H/5                                         #Position initale de la particule 1 au sein du tube
+pr0=H/3                                           #Position initale de la particule 1 au sein du tube
 pr02=H/3                                          #Position initale de la particule 2 au sein du tube
 pr=rc+pr0                                         #Position initale de la particule 1 
 pr2=rc+pr02                                       #Position initale de la particule 2
@@ -141,7 +141,6 @@ def integrale(y,t,rho,Umax,H,r,mu,Uf,rc):
     i=int(10*np.pi/t)                                               #indice indiquant l'avancement de la spirale
     pr,p=y
     dydt=[p,Fd(mu,r,rc[i],rho,Uf,H)-Fl(rho,Umax,H,r,pr0)]
-    print(dydt)
     return dydt
    
 
@@ -159,15 +158,22 @@ def main(n):
         x=tab1*np.cos(tab3)
         y=tab1*np.sin(tab3)
         x2=tab2*np.cos(tab3)
-        plt.plot(x,y)
+        plt.plot(x,y,"g")
     if n==2 :                                                       #Utilisation de la méthode intégrale
         t=np.linspace(10*np.pi,0,num=10001)
         rc2=(a)*((t*t+1)**(3/2))/(t*t+2)
         y0=[pr0,0.0]
-        sol=odeint(integrale,y0,t,args=(rho,Umax,H,r,mu,Uf,rc2))
-        x=(sol[:,0]+rc2)*np.cos(t)
-        y=(sol[:,0]+rc2)*np.sin(t)
-        plt.plot(x,y,"r")
+        sol1=odeint(integrale,y0,t,args=(rho,Umax,H,r,mu,Uf,rc2))
+        sol2=odeint(integrale,y0,t,args=(rho,Umax,H,r2,mu,Uf,rc2))
+        x1=(sol1[:,0]+rc2)*np.cos(t)
+        y1=(sol1[:,0]+rc2)*np.sin(t)
+        #x=t
+        #y=sol1[:,0]
+        #y2=sol2[:,0]
+        x2=(sol2[:,0]+rc2)*np.cos(t)
+        y2=(sol2[:,0]+rc2)*np.sin(t)
+        plt.plot(x1,y1,"r")
+        plt.plot(x2,y2,"b")
 
 main(1)
 main(2)
